fcb38a1366418d2a4ed37812f1b0b8572bcfb1e3
Florian Walbaum <walbaum@fastec.de>
2015-12-01 16:24:28 +0100

    Fixed assembly info.

--- SimSharp/Properties/AssemblyInfo.cs
+++ SimSharp/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fastec.SimSharp")]
+[assembly: AssemblyTitle("SimSharp")]
 [assembly: AssemblyDescription("A .NET port of SimPy, discrete event simulation framework")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("SimSharp")]
