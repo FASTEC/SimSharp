d0f574a3107552a1371c1357ee1972fc9a47a0a9
Florian Walbaum <walbaum@fastec.de>
2015-12-03 14:37:36 +0100

    Updated package version to alpha002.

--- SimSharp/Properties/AssemblyInfo.cs
+++ SimSharp/Properties/AssemblyInfo.cs
@@ -48,5 +48,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyInformationalVersion( "1.0.0-alpha001" )]
+[assembly: AssemblyInformationalVersion( "1.0.0-alpha002" )]
 [assembly: AssemblyFileVersion("1.0.0.0")]
